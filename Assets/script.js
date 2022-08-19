6e87f5698fac982aa24bd4060db399b55c47f267
John Bynum <iexclusiveproductions@gmail.com>
2022-08-19 14:59:45 -0400

    Updated Syntax

--- Assets/script.js
+++ Assets/script.js
@@ -53,6 +53,7 @@ let weather = {
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
     document.querySelector(".city").innerText = "Weather in " + name;
+    document.getElementById('firstSearch').innerText = name;
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
